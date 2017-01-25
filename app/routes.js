c34d6e96b1234e35239faaf7729d1f553f09728e
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-01-26 00:36:51 +0700

    edit route for index and add routes to signup

--- app/routes.js
+++ app/routes.js
@@ -2,20 +2,26 @@ const User = require('./models/user')
 
 module.exports = function(app){
 	app.get('/', function(req,res){
-		res.send('hello,world');
+		res.render('index.ejs');
 	});
 
-	app.get('/:username/:password', function(req,res){
-		const newUser = new User();
-		newUser.local.username = req.params.username;
-		newUser.local.password = req.params.password;
+	app.get('/signup', function(req,res){
+		res.render('signup.ejs',{
+			message: 'Victory'
+		});
+	});
 
-		console.log(newUser.local.username + ' ' + newUser.local.password);
+	app.post('/signup', function(req,res){
+		const newUser = new User();
+		newUser.local.username = req.body.email;
+		newUser.local.password = req.body.password;
+
 		newUser.save(function(err){
 			if(err){
 				throw err;
 			}
 		});
-		res.send('Success');
-	});
+		res.redirect('/');
+	});	
+
 }
